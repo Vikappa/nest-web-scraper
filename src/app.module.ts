f5086014fc568df9d7f3982c4624cd80eeb69580
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-08-19 13:39:38 +0200

    scraper controller added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScarperController } from './scarper/scarper.controller';
+import { ScraperController } from './scraper/scraper.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ScarperController, ScraperController],
   providers: [AppService],
 })
 export class AppModule {}
