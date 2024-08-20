3b9dfb641c2cf9dccfa556a26ff5fb38aa6092a8
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-08-20 20:16:43 +0200

    Update Cors Policy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://spidernest.vercel.app', // Consente solo richieste da questo dominio
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
